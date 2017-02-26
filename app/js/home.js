2d043a0df64afb28a36ba94bd63d8f3eda53f9c9
Sebastián Rojas <sebastian@iridian.co>
2017-02-26 17:58:23 -0500

    Maqueta reunion

--- app/js/home.js
+++ app/js/home.js
@@ -13,28 +13,56 @@ $(function () {
         }
     });
 
-    $('#colombia').mapster('unbind').mapster({
-        clickNavigate: true,
-            render_highlight: {
-            altImageOpacity: 1,
-            fillOpacity: 1,
-            fillColor: 'ffffff',
-            altImage: 'img/map-highlight.png',
-            fadeDuration: 100
-        },
-            mapKey: 'data-key',
-            areas: [
-        {
-            key: 'north',
-            isSelectable: false
-        },
-        {
-            key: 'center',
-            isSelectable: false
-        },
-        {
-            key: 'south',
-            isSelectable: false
-        }]
+    var mapster = function () {
+
+        //$('#colombia').mapster('resize');
+
+        //$('#colombia-container').append('<img id="colombia" src="img/map.png" alt="" usemap="#colombia-regions" class="img-fluid">');
+
+        $('#colombia').mapster({
+            clickNavigate: true,
+                render_highlight: {
+                altImageOpacity: 1,
+                fillOpacity: 1,
+                fillColor: 'ffffff',
+                altImage: 'img/map-highlight.png',
+                fadeDuration: 100
+            },
+                mapKey: 'data-key',
+                areas: [
+                {
+                    key: 'north',
+                    isSelectable: false
+                },
+                {
+                    key: 'center',
+                    isSelectable: false
+                },
+                {
+                    key: 'south',
+                    isSelectable: false
+                }
+            ]
+        });
+    }
+
+    mapster();
+
+    var doit;
+
+    $(window).resize(function() {
+        
+        //TODO
+
+        clearTimeout(doit);
+
+        doit = setTimeout(function () {
+
+            console.log('resize')
+
+            $('#mapster_wrap_0').css('width', '')
+
+            $('#colombia').mapster('resize', $('#mapster_wrap_0').width());
+        }, 200);
     });
 })
